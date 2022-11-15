fbdfa9f8969699fc67dd9c86eb2878a2adb4a5ca
Zsomi4 <k72302872172@ktch.hu>
2022-11-15 18:48:23 +0100

    update

--- olvaso.py
+++ olvaso.py
@@ -1,29 +1,32 @@
+from employee import Employee
 class App:
     def __init__(self):                 #konstruktor
         self.fileName = 'dolgozok.txt'
+        self.employeeList = []
 
     def readFile(self):                 #metódus
         fp = open(self.fileName, 'r', encoding='UTF-8')
         self.lines = fp.readlines()
+
         for line in self.lines:
+            (id, name, city, address, salary, birth) = line.split(':')
+            employee = Employee(id, name, city, address, salary, birth)
+            self.employeeList.append(employee)
             #print(line.rstrip()) - törli a sorvégi whitespace-eket
-            pass
 
         fp.close()
         
     def sumSalary(self):
         sum = 0
-        for line in self.lines:
-           #lineArray = line.split(':')
-           (id, name, city, address, salary, enter) = line.split(':')
+        for employee in self.employeeList:
            #print(int(salary))
-           if city == 'Miskolc':                #szelektáljuk feltételek aélapján
-                sum = sum + int(salary)
+           if employee.city == 'Miskolc':                #szelektáljuk feltételek aélapján
+                sum = sum + int(employee.salary)
 
         print('Összeg:', sum)
           
         
 
-app = App()                             #objektum /app/ - konstruktor meghívása
+app = App()                           #objektum /app/ - konstruktor meghívása
 app.readFile()
 app.sumSalary()
