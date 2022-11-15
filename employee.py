6d58b9113fc410a689e604a0351642e8268c3e18
Zsomi4 <k72302872172@ktch.hu>
2022-11-15 18:33:15 +0100

    employee

---
+++ employee.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self, id, name, city, address, salary, birth):
+        self.id = id
+        self.name = name
+        self.city = city
+        self.address = address
+        self.salary = salary
+        self.birth = birth
